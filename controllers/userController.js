e2bbd2ffdca39a552fe7e4b558110ff0bb642fa6
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-16 13:49:37 +0530

    update user Controler

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ import User from "../models/user.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import nodemailer from 'nodemailer'
+import Otp from "../models/otp.js";
 
 dotenv.config();
 
@@ -27,8 +29,17 @@ export function postUsers(req, res) {
       return newUser.save();
     })
     .then(() => {
+      const otp=Math.floor(1000+Math.random()*9000)
+      const newOtp=new Otp({
+        email:user.email,
+        otp:otp
+      })
+      newOtp.save().then(()=>{
+        sendSampleEmail(user.email.otp)
+      })
       res.status(201).json({
-        message: "User created successfully",
+        message: "User created successfully"
+
       });
     })
     .catch((error) => {
@@ -123,4 +134,95 @@ export function getUser(req, res) {
     message: "User found",
     user: req.user,
   });
+}
+export function sendSampleEmail(email,otp){
+  const email=req.body.email
+  const transport = nodemailer.createTransport({
+        service:"gmail",
+        host:"smtp.gmail.com",
+        port:587,
+        secure:false,
+        auth:{
+          user:process.env.EMAIL,
+          pass:process.env.PASSWORD
+        }
+
+  })
+  const message={
+    from:process.env.EMAIL,
+    to:email,
+    subject:"Sample Email",
+    text:"This is a sample email."
+  }
+  transport.sendMail(message,(err,info)=>{
+    if(err){
+      console.log(err)
+    }else{
+      console.log(info)
+    }
+  })
+}
+export function deleteUserByEmail(req,res){
+  if(!isAdminValid(req)){
+    res.status(403).json({
+      message:"Forbidden"
+    })
+    return
+  }
+  const email=req.params.email
+  User.findOneAndDelete({email:email})
+  .then(()=>{
+    res.json({
+      message:"User Deleted"
+    })
+  }).catch((err)=>{
+    res.json({
+      message:"User delete failed",
+      error:err
+    })
+  })
+}
+export function disableUser(req,res){
+   if(!isAdminValid(req)){
+    res.status(403).json({
+      message:"Forbidden"
+    })
+    return
+  }
+  const userId=req.params.userid;
+  const disabled=req.body.disabled
+
+  User.findOneAndUpdate({_id:userId},{disabled:disabled})
+  .then(()=>{
+    res.json({
+      message:"User disabled/enabled"
+    })
+  }).catch((err)=>{
+    res.json({
+      message:"User disable/enable failed",
+      error:err
+    })
+  })
+}
+export function changeUserType(req,res){
+  if(!isAdminValid(req)){
+    res.status(403).json({
+      message:"Forbidden"
+    })
+    return
+  }
+  const userId = req.params.userId
+  const type=req.body.type
+
+  User.findOneAndUpdate({_id:userId},{disabled:disabled})
+  .then(()=>{
+    res.json({
+      message:"User type updated"
+    })
+  }).catch((err)=>{
+    res.json({
+      message:"User type update failed",
+      error:err
+    })
+  })
 }

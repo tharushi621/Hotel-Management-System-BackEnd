bcd25efc20eb718bc42e17b5cf8c5b6b83da9611
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-16 13:50:16 +0530

    update booking Routes

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,8 +1,12 @@
 import express from 'express'
-import { createBooking } from '../controllers/bookingController.js'
+import {getAllBookings, createBooking, deleteBooking, retrieveBookingByDate, createBookingUsingCategory } from '../controllers/bookingController.js'
 
 const bookingRouter= express.Router()
 
 bookingRouter.post("/",createBooking)
+bookingRouter.get("/",getAllBookings)
+bookingRouter.post("/filter-date",retrieveBookingByDate)
+bookingRouter.post("/create-by-category",createBookingUsingCategory)
+bookingRouter.delete("/:id", deleteBooking)
 
 export default bookingRouter

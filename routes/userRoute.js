c5c7d38628bd82a513a7bae659b3d67e68d6202f
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-16 13:49:23 +0530

    update userRoutes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import {getUser, loginUser, postUsers} from '../controllers/userController.js'
+import {getUser, loginUser, postUsers, sendSampleEmail} from '../controllers/userController.js'
 
 const userRouter = express.Router();
 
 userRouter.post("/",postUsers)
 userRouter.post("/login",loginUser)
 userRouter.get("/",getUser)
+userRouter.post("/email",sendSampleEmail)
 
 
 export default userRouter

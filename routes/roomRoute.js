1c0116445b2f697a84164d7f52a23cb8db8e3950
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-17 01:02:40 +0530

    update room routes

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,13 +1,21 @@
-import express from 'express'
-import { createRoom, deleteRoom, findRoomById, getRooms, getRoomsByCategory, updateRoom } from '../controllers/roomControllers.js'
+import express from "express";
+import {
+  createRoom,
+  deleteRoom,
+  findRoomById,
+  getRooms,
+  getRoomsByCategory,
+  updateRoom,
+} from "../controllers/roomControllers.js";
 
-const roomRouter = express.Router()
+const roomRouter = express.Router();
 
-roomRouter.post("/",createRoom)
-roomRouter.delete("/:roomId",deleteRoom)
-roomRouter.get("/",getRooms)
-roomRouter.get("/by-category/:category",getRoomsByCategory)
-roomRouter.get("/:roomId",findRoomById)
-roomRouter.put("/:roomId",updateRoom)
+// Routes
+roomRouter.post("/", createRoom);
+roomRouter.delete("/:roomId", deleteRoom);
+roomRouter.get("/", getRooms);
+roomRouter.get("/:category", getRoomsByCategory); // updated to match frontend
+roomRouter.get("/:roomId", findRoomById);
+roomRouter.put("/:roomId", updateRoom);
 
-export default roomRouter
+export default roomRouter;

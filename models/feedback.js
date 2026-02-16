7c8d82f471257b6b4aa2e2bf36b7cfd56434869e
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-17 01:02:52 +0530

    create feedback.js

---
+++ models/feedback.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const feedbackSchema = mongoose.Schema(
+  {
+    bookingId: {
+      type: Number,
+      required: true
+    },
+
+    roomId: {
+      type: Number,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true
+    },
+
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5
+    },
+
+    comment: {
+      type: String,
+      required: true
+    },
+
+    status: {
+      type: String,
+      default: "Visible" // Visible / Hidden (admin control)
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now
+    }
+  }
+);
+
+const Feedback = mongoose.model("Feedbacks", feedbackSchema);
+export default Feedback;

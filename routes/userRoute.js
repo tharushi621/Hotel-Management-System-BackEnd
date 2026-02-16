3d956e8a86e461aef57babfa94642fa8d4e9e4bd
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-17 01:02:31 +0530

    update user Routes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,25 @@
-import express from 'express'
-import {getUser, loginUser, postUsers, sendSampleEmail} from '../controllers/userController.js'
+import express from "express";
+import { 
+  postUsers,
+  loginUser,
+  getUser,
+  verifyUserEmail,
+  deleteUserByEmail,
+  disableUser,
+  changeUserType,
+  getAllUsers
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/",postUsers)
-userRouter.post("/login",loginUser)
-userRouter.get("/",getUser)
-userRouter.post("/email",sendSampleEmail)
+// User routes
+userRouter.post("/", postUsers);
+userRouter.post("/login", loginUser);
+userRouter.get("/", getUser);
+userRouter.post("/all", getAllUsers); // POST allows page & limit in body
+userRouter.put("/change-type/:userId", changeUserType);
+userRouter.put("/disable/:userId", disableUser);
+userRouter.delete("/admin-delete/:email", deleteUserByEmail);
+userRouter.post("/verify-email", verifyUserEmail);
 
-
-export default userRouter
+export default userRouter;

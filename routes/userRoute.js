7e72bbe66d2bf75e276dda7e70c5f83abcfebc4d
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-17 23:22:26 +0530

    update userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,25 +1,28 @@
 import express from "express";
-import { 
+import {
+  getAllUsers,
   postUsers,
   loginUser,
   getUser,
-  verifyUserEmail,
-  deleteUserByEmail,
+  deleteUserById, // updated
   disableUser,
   changeUserType,
-  getAllUsers
+  verifyUserEmail,
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
-// User routes
-userRouter.post("/", postUsers);
+// Admin routes
+userRouter.post("/all", protect, getAllUsers);
+userRouter.delete("/admin-delete/:id", protect, deleteUserById);
+
+// Other user routes
+userRouter.post("/register", postUsers);
 userRouter.post("/login", loginUser);
-userRouter.get("/", getUser);
-userRouter.post("/all", getAllUsers); // POST allows page & limit in body
-userRouter.put("/change-type/:userId", changeUserType);
-userRouter.put("/disable/:userId", disableUser);
-userRouter.delete("/admin-delete/:email", deleteUserByEmail);
+userRouter.get("/me", protect, getUser);
 userRouter.post("/verify-email", verifyUserEmail);
+userRouter.patch("/disable/:userId", protect, disableUser);
+userRouter.patch("/type/:userId", protect, changeUserType);
 
 export default userRouter;

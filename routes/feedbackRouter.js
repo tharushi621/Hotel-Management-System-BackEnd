5aaec3a26af8b13b02fe4c7c2cea19c6980d717f
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 00:22:11 +0530

    update feedback route

--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { createFeedback,getAllFeedbacks,getMyFeedbacks,deleteFeedback,updateFeedbackStatus } from "../controllers/feedbackController.js";
+import {
+  createFeedback,
+  getAllFeedbacks,
+  getMyFeedbacks,
+  deleteFeedback,
+  updateFeedbackStatus
+} from "../controllers/feedbackController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const feedbackRouter = express.Router();
 
-feedbackRouter.post("/", createFeedback);
-feedbackRouter.get("/", getAllFeedbacks);
-feedbackRouter.get("/my", getMyFeedbacks);
-feedbackRouter.delete("/:id", deleteFeedback);
-feedbackRouter.put("/:id/status", updateFeedbackStatus);
+// Customer routes (requires login)
+feedbackRouter.post("/", protect, createFeedback);
+feedbackRouter.get("/my", protect, getMyFeedbacks);
+
+// Admin routes (requires login + admin)
+feedbackRouter.get("/", protect, getAllFeedbacks);
+feedbackRouter.delete("/:id", protect, deleteFeedback);
+feedbackRouter.put("/:id/status", protect, updateFeedbackStatus);
 
 export default feedbackRouter;

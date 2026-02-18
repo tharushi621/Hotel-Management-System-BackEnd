c9888d7f77bc853b46cb261dad5b203e47ad32f9
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 00:22:31 +0530

    update feedback.js

--- models/feedback.js
+++ models/feedback.js
@@ -6,34 +6,28 @@ const feedbackSchema = mongoose.Schema(
       type: Number,
       required: true
     },
-
     roomId: {
       type: Number,
       required: true
     },
-
     email: {
       type: String,
       required: true
     },
-
     rating: {
       type: Number,
       required: true,
       min: 1,
       max: 5
     },
-
     comment: {
       type: String,
       required: true
     },
-
     status: {
       type: String,
       default: "Visible" // Visible / Hidden (admin control)
     },
-
     createdAt: {
       type: Date,
       default: Date.now

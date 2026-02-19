4ccd43f41ddd30365ba919ae1a49af100fbd8876
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:32:46 +0530

    update booking.js

--- models/booking.js
+++ models/booking.js
@@ -1,49 +1,46 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const bookingSchema = mongoose.Schema(
-    {
-        bookingId :{
-            type:Number,
-            required :true,
-            unique:true
-        },
-        roomId:{
-            type:Number,
-            required:true
-        },
-        email:{
-            type:String,
-            required:true
-        },
-        status:{
-            type:String,
-            required:true,
-            default:"Pending"
-        },
-        reason:{
-            type:String,
-            default:""
-        },
-        start:{
-            type:Date,
-            required:true
-        },
-        end:{
-            type:Date,
-            required:true
-        },
-        timeStamp:{
-            type:Date,
-            default:Date.now
-        },
-        notes: {
-            type: String,
-            default: ""
-}
+const bookingSchema = mongoose.Schema({
+  bookingId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  roomId: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "Pending",
+  },
+  reason: {
+    type: String,
+    default: "",
+  },
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+  timeStamp: {
+    type: Date,
+    default: Date.now,
+  },
+  notes: {
+    type: String,
+    default: "",
+  },
+});
 
-    }
-)
+const Booking = mongoose.model("Bookings", bookingSchema);
 
-const Booking = mongoose.model("Bookings",bookingSchema)
-
-export default Booking
+export default Booking;

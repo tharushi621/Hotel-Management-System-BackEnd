e1d061e53f709f1aeb93fc8dde370854da89c347
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:33:23 +0530

    update room.js

--- models/room.js
+++ models/room.js
@@ -1,40 +1,38 @@
 import mongoose from "mongoose";
 
-const roomSchema = mongoose.Schema(
+const roomSchema = mongoose.Schema({
+  roomId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  maxGuests: {
+    type: Number,
+    required: true,
+    default: 3,
+  },
+  available: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  photos: [
     {
-        roomId:{
-            type:Number,
-            required : true,
-            unique:true
-        },
-        category:{
-            type:String,
-            required : true
-        },
-        maxGuests:{
-            type:Number,
-            required : true,
-            default:3
-        },
-        available:{
-            type:Boolean,
-            required : true,
-            default:true
-        },
-        photos:[
-            {
-            type:String
-            }
-        ],
-        specialDescription:{
-            type:String,
-            default : ""
-        },
-        notes:{
-            type:String,
-            default : ""
-        }
-    }
-)
-const Room =mongoose.model("Rooms",roomSchema)
-export default Room
+      type: String,
+    },
+  ],
+  specialDescription: {
+    type: String,
+    default: "",
+  },
+  notes: {
+    type: String,
+    default: "",
+  },
+});
+const Room = mongoose.model("Rooms", roomSchema);
+export default Room;

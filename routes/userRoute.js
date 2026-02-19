670f08724fb6ce62e2f5a24138557ea06114e7c7
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:34:14 +0530

    update user router

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -14,14 +14,14 @@ import { protect } from "../middleware/authMiddleware.js";
 const userRouter = express.Router();
 
 //Admin Routes
-userRouter.get("/all", protect, getAllUsers);               
-userRouter.delete("/delete/:id", protect, deleteUserById);  
-userRouter.patch("/disable/:userId", protect, disableUser); 
-userRouter.patch("/type/:userId", protect, changeUserType); 
-//Public / User Routes 
-userRouter.post("/register", postUsers);          
-userRouter.post("/login", loginUser);            
-userRouter.post("/verify-email", verifyUserEmail); 
-userRouter.get("/me", protect, getUser);           
+userRouter.get("/all", protect, getAllUsers);
+userRouter.delete("/delete/:id", protect, deleteUserById);
+userRouter.patch("/disable/:userId", protect, disableUser);
+userRouter.patch("/type/:userId", protect, changeUserType);
+//Public / User Routes
+userRouter.post("/register", postUsers);
+userRouter.post("/login", loginUser);
+userRouter.post("/verify-email", verifyUserEmail);
+userRouter.get("/me", protect, getUser);
 
 export default userRouter;

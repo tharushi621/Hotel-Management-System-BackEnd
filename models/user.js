93979fa47bb1906ca8b800bacf8a05a95d716ff8
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:33:29 +0530

    update user.js

--- models/user.js
+++ models/user.js
@@ -1,50 +1,48 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema(
-    {
-        email:{
-            type:String,
-            required:true,
-            unique:true,          
-        },
-        password:{
-            type:String,
-            required:true
-        },
-        firstName:{
-            type:String,
-            required:true
-        },
-        lastName:{
-            type:String,
-            required:true
-        },
-        type:{
-            type:String,
-            required:true,
-            default:"user"
-        },
-        whatsApp:{
-            type:String,
-            required:true
-        },
-        phone:{
-            type:String,
-            required:true
-        },
-        disabled:{
-            type:Boolean,
-            required:true,
-            default:false
-        },
-        emailVerified:{
-            type:Boolean,
-            required:true,
-            default:false
-        }
-    }
-)
+const userSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    default: "user",
+  },
+  whatsApp: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  disabled: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  emailVerified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
 
-const User = mongoose.model("users",userSchema)
+const User = mongoose.model("users", userSchema);
 
 export default User;

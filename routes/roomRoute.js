47b8a483b373f5c015e0b779711a8b51388e9eb0
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:34:07 +0530

    update room router

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -1,17 +1,24 @@
 import express from "express";
-import { createRoom,deleteRoom,updateRoom,getRoomsByCategory,findRoomById,getRooms } from "../controllers/roomControllers.js";
+import {
+  createRoom,
+  deleteRoom,
+  updateRoom,
+  getRoomsByCategory,
+  findRoomById,
+  getRooms,
+} from "../controllers/roomControllers.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const roomRouter = express.Router();
 
 // Admin Routes
-roomRouter.post("/", protect, createRoom);           
-roomRouter.delete("/:roomId", protect, deleteRoom); 
-roomRouter.patch("/:roomId", protect, updateRoom); 
+roomRouter.post("/", protect, createRoom);
+roomRouter.delete("/:roomId", protect, deleteRoom);
+roomRouter.patch("/:roomId", protect, updateRoom);
 
 // Public Routes
 roomRouter.get("/category/:category", getRoomsByCategory);
-roomRouter.get("/:roomId", findRoomById);        
-roomRouter.get("/", getRooms);                     
+roomRouter.get("/:roomId", findRoomById);
+roomRouter.get("/", getRooms);
 
 export default roomRouter;

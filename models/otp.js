d5c556167a04e7123da643df8cb8ca80cac7a386
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:33:17 +0530

    update otp.js

--- models/otp.js
+++ models/otp.js
@@ -1,20 +1,18 @@
 import mongoose from "mongoose";
 
-const otpSchema=mongoose.Schema(
-    {
-        email:{
-            type:String,
-            requires:true
-        },
-        otp:{
-            type:Number,
-            required:true
-        },
-        date:{
-            type:Date,
-            default:Date.now
-        }
-    }
-)
-const Otp=mongoose.model("otp",otpSchema)
+const otpSchema = mongoose.Schema({
+  email: {
+    type: String,
+    requires: true,
+  },
+  otp: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const Otp = mongoose.model("otp", otpSchema);
 export default Otp;

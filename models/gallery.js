35932463bd2fb079a90bc07cd7e49b219e7c383f
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-19 19:33:10 +0530

    update gallery.js

--- models/gallery.js
+++ models/gallery.js
@@ -1,25 +1,23 @@
 import mongoose from "mongoose";
 
-const gallerySchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        image: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        category: {
-            type: String,
-            default: ""
-        }
-    }
-);
+const gallerySchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    default: "",
+  },
+});
 
 const GalleryItem = mongoose.model("galleryItems", gallerySchema);
 

f99c0808a9c5caaf20945d6bde799b6b452645cd
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-21 23:16:56 +0530

    update user route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -9,22 +9,28 @@ import {
   changeUserType,
   verifyUserEmail,
   resendOtp,
+  forgotPassword,
+  resetPassword,
   testEmail,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const userRouter = express.Router();
 
-//Admin Routes
+// Admin Routes
 userRouter.get("/all", protect, getAllUsers);
 userRouter.delete("/delete/:id", protect, deleteUserById);
 userRouter.patch("/disable/:userId", protect, disableUser);
 userRouter.patch("/type/:userId", protect, changeUserType);
 
-//Public / User Routes
+// Public / User Routes
 userRouter.post("/register", postUsers);
 userRouter.post("/login", loginUser);
 userRouter.post("/verify-email", verifyUserEmail);
 userRouter.post("/resend-otp", resendOtp);
+userRouter.post("/forgot-password", forgotPassword);
+userRouter.post("/reset-password", resetPassword);
 userRouter.get("/me", protect, getUser);
+userRouter.get("/test-email", testEmail);
+
 export default userRouter;

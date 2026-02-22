b3c727087163587ebbc368348c13ba77fce9387e
Tharushi Rathnasekara <tharubhagya621@gmail.com>
2026-02-22 21:20:56 +0530

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,9 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
